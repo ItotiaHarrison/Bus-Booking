2120b3b8ee47a39fa8f8359f4129beb36275db60
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-05 13:32:46 +0300

    pass props

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-
+function Search({search, setSearch}){
+    function handleSearch(event){
+        setSearch(event.target.value);
+    }
 
   return (
     <div className="ui large fluid icon input">
-    
+    <input type="text" name={search} placeholder="Search" onChange={handleSearch}/>
     </div>
   )
 }
