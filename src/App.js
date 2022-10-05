4a12740cd92adfd7af2209b9ec5b579293195da1
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-05 10:50:23 +0300

    import home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-    
+    <NavBar />
     </div>
   );
 }
