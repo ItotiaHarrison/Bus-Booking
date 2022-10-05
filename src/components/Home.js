daa69c3e2b356c0ba7600bb37b1e946dcdf0657c
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-05 13:43:14 +0300

    set buslist state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Search from './Search';
 import BusList from './BusList';
 
-function Home(){
-    const [search, setSearch] = useState("")
+function Home() {
+    const [search, setSearch] = useState("");
+    const [list, setList] = useState([]);
 
 
     return (
         <div>
             <h1>HOME</h1>
             <Search search={search} setSearch={setSearch} />
-            <BusList/>
+            <BusList busList={list} setBusList={setList} />
         </div>
     )
 }
