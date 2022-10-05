3885e132d55f10a920b089fb50caef07c9429418
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-05 13:57:03 +0300

    fetch data

--- src/components/BusList.js
+++ src/components/BusList.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function BusList(){
-  return (
-    <div>BusList</div>
-  )
+function BusList({ busList, setBusList }) {
+    useEffect(() => {
+        fetch("http://localhost:3000")
+        .then((res) => res.json())
+        .then((data) => setBusList(data))
+    }, []);
+
+    return (
+        <div>BusList</div>
+    )
 }
 
 export default BusList;
