f4eb71f04415d5a1bc4f8d5aea2e993bcaead031
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-05 11:35:56 +0300

    login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-function Login (){
-  return (
-    <div>Login</div>
-  )
+function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <div>
+                    <input type="text" name="username" placeholder="Username" />
+                </div>
+
+                <div>
+                    <input type="password" name="password" placeholder="Password" />
+                </div>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
 }
 
 export default Login;
