1624867246fd8483dbf70bfdb0cefecf6979db11
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-05 11:57:33 +0300

    import navlink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import About from './About';
@@ -7,6 +8,34 @@ import Support from './Support';
 function NavBar() {
     return (
         <div>
+            <NavLink
+            to="/"
+            exact
+            >
+                Home
+            </NavLink>
+
+            <NavLink
+            to="/login"
+            exact
+            >
+                Login
+            </NavLink>
+
+            <NavLink
+            to="/about"
+            exact
+            >
+                About
+            </NavLink>
+
+            <NavLink
+            to="/support"
+            exact
+            >
+                Support
+            </NavLink>
+
             <Home />
             <Login />
             <About />
