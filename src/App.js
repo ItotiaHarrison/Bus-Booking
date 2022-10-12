b470271c1d5c129bc08d0045f9982f0531dfb8c6
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-13 01:32:53 +0300

    landing page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from './components/Footer';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [userName, setUserName] = useState("");
+  
 
   return (
     <div className="App">
