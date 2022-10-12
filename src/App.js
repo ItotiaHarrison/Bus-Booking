ddb804692fab821dc7e8ac21730ec55550dd468c
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-13 01:50:24 +0300

    set the Landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, {useState} from 'react';
 import { Route, Switch } from "react-router-dom";
+import LandingPage from './components/LandingPage';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -18,13 +19,14 @@ function App() {
     <div className="App">
       <NavBar setIsLoggedIn={setIsLoggedIn} />
       <Switch>
+      <Route path="/" exact component={LandingPage} />
         <Route exact path="/hire">
           <Hire setIsLoggedIn={setIsLoggedIn} />
         </Route>
         <Route exact path="/login">
           <Login setIsLoggedIn={setIsLoggedIn} />
         </Route>
-        <Route exact path="/">
+        <Route exact path="/home">
           <Home isLoggedIn={isLoggedIn} />
         </Route>
         <Route exact path="/support">
